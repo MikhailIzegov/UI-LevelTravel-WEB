4760b88f2a86fa6b254e6d7c58f85d54c4f9a49e
MikhailIzegov <mikhail.izegov@yandex.ru>
2024-04-03 21:28:15 +0300

    added allure and jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,50 +1,46 @@
 import os
-
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selene import browser
 from dotenv import load_dotenv
-# from tests import attach
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
-# @pytest.fixture(scope='session', autouse=True)
-# def load_env():
-#     load_dotenv()
+@pytest.fixture(scope='session', autouse=False)
+def load_env():
+    load_dotenv()
 
 
-# @pytest.fixture(scope="function", autouse=False)
-# def setup_browser():
+@pytest.fixture(scope="function", autouse=True)
+def setup_browser(load_env):
+    options = Options()
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': '100.0',
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
 
-#   @pytest.fixture(scope="function", autouse=True)
-#   def setup_browser():
-#     browser.config.base_url = 'https://demoqa.com'
-#     options = Options()
-#     selenoid_capabilities = {
-#         'browserName': 'chrome',
-#         'browserVersion': '100.0',
-#         'selenoid:options': {
-#             'enableVNC': True,
-#             'enableVideo': True
-#         }
-#     }
-#     options.capabilities.update(selenoid_capabilities)
-#
-#     login = os.getenv('LOGIN')
-#     password = os.getenv('PASSWORD')
-#
-#     driver = webdriver.Remote(
-#         command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
-#         options=options
-#     )
-#
-#     browser.config.driver = driver
-#
-#     yield
-#
-#     attach.add_screenshot(browser)
-#     attach.add_logs(browser)
-#     attach.add_html(browser)
-#     attach.add_video(browser)
-#
-#     browser.quit()
+    selenoid_login = os.getenv('SELENOID_LOGIN')
+    selenoid_password = os.getenv('SELENOID_PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{selenoid_login}:{selenoid_password}@selenoid.autotests.cloud/wd/hub',
+        options=options
+    )
+
+    browser.config.driver = driver
+
+    yield
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
